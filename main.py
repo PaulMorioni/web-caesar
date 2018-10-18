50e7fd59dc8e335564e91b633b78b3223e097a5a
PAUL MORIONI <Morionipaul@gmail.com>
2018-10-17 19:53:13 -0500

    Finished product, this version outputs into the textbox

--- main.py
+++ main.py
@@ -11,26 +11,26 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
         <form method="post">
             <label for="rot">Rotate by:</label>
             <input type="text" name="rot" id="rot" value="0">
-            <textarea rows="10" cols="30" id="box" name="box"></textarea>
+            <textarea rows="10" cols="30" id="box" name="box">{0}</textarea>
             <input type="submit" value="Submit">
 
         </form>
@@ -42,11 +42,12 @@ def encrypt():
     text = str(request.form['box'])
     rot = int(request.form['rot'])
     encrypted = rotate_string(text,rot)
-    return "<h1>" + encrypted + "</h1>"
+    return form.format(encrypted)
 
 @app.route("/")
 def index():
-    return form
+    empty_string = ""
+    return form.format(empty_string)
     
     
 
